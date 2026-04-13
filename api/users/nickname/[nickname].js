ea092ca22b28e60d8593003c1c0ad352275ac427
markizow <127435201+Switov69@users.noreply.github.com>
2026-04-14 00:20:01 +0300

    213

--- api/users/nickname/[nickname].js
+++
@@ -1,9 +0,0 @@
-import { getPool, err } from '../../_db.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return err(res, 405, 'Method not allowed');
-  const pool = getPool();
-  const { rows } = await pool.query('SELECT id FROM users WHERE LOWER(nickname) = LOWER($1)', [req.query.nickname]);
-  if (!rows[0]) return err(res, 404, 'Not found');
-  res.json({ exists: true });
-}
